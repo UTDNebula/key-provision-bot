5e325d18684e055b5cd0129e2173c8c8276c28e9
mikehquan19 <hoangphucquan19@gmail.com>
2026-05-24 20:08:38 -0500

    Lint and minor change

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,13 +1,10 @@
-import {
-  REST,
-  Routes,
-} from "discord.js";
+import { REST, Routes } from "discord.js";
 import { getCommands } from "./utils.ts";
 import "dotenv/config";
 
-const serializedCmds = [];
+const serialized = [];
 for (const command of await getCommands()) {
-  serializedCmds.push(command.data.toJSON());
+  serialized.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -16,13 +13,13 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN ?? "");
 // Deploy commands
 (async () => {
   try {
-    console.log(`Reloading ${serializedCmds.length} application commands...`);
+    console.log(`Reloading ${serialized.length} application commands...`);
 
     // Refresh all commands
     const data: any = await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID ?? ""),
       {
-        body: serializedCmds,
+        body: serialized,
       },
     );
 
